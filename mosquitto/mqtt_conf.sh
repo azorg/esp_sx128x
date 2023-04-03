86a2dd828093fa49f15d5e97a3c9afe89ac8c592
Alex Zorg <azorg@mail.ru>
2023-04-03 16:25:53 +0300

    save

--- mosquitto/mqtt_conf.sh
+++ mosquitto/mqtt_conf.sh
@@ -8,7 +8,7 @@ MQTT_QOS="2"
 
 MQTT_KEEPALIVE="60" # by default 60 sec
 
-#MQTT_INSECURE="yes" # don't use client TLS cert
+MQTT_INSECURE="yes" # don't use client TLS cert
 MQTT_USER="mqtt"
 MQTT_PASSWD="12345678" # secret password of user
 MQTT_CLIENT_ID="id"
